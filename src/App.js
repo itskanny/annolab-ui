45d55447befaec917d0a89541fef3f8372866613
ZainHaderArif <Zainuhaider64@gmail.com>
2022-03-08 07:52:33 +0000

    Evaluation till router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {Col, Layout} from 'antd';
 import './App.less';
+import './App.css'
 import {Content} from "antd/es/layout/layout";
 import SetNewPassword from './Pages/ForgetPassword/SetNewPassword';
 import Navbar from "./Components/Ui/Navbar/Navbar";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import ImportProject from "./Pages/ImportProject/ImportProject";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Invite from "./Pages/Invite/Invite";
+import RecoverPassword from "./Pages/ForgetPassword/RecoverPassword";
+import ExportProject from "./Pages/ExportProject/ExportProject";
+import AddProject from "./Pages/AddProject/AddProject";
+import UpdateProfile from "./Pages/AccountSteps/UpdateProfile";
+import CreateTeam from "./Pages/AccountSteps/CreateTeam";
+import CreateProject from "./Pages/AccountSteps/CreateProject";
+import CreateOrganization from "./Pages/AccountSteps/CreateOrganization";
+import AddImage from "./Pages/AccountSteps/AddImage";
+import List from "./Pages/List/List";
+
 
 const App = () => (
-    <Layout className={"tw-h-full"}>
+    <>
+<List></List>
 
-        <Navbar/>
-        <Content>
-            <SetNewPassword/>
-        </Content>
-    </Layout>
+        <Router>
+
+
+
+
+                <Route path="/addimage">
+                    <AddImage></AddImage>
+                </Route>
+                <Route path="/createorganization">
+                    <CreateOrganization></CreateOrganization>
+                </Route>
+                <Route path="/createproject">
+                    <CreateProject></CreateProject>
+                </Route>
+                <Route path="/createteam">
+                    <CreateTeam></CreateTeam>
+                </Route>
+                <Route path="/updateprofile">
+                    <UpdateProfile></UpdateProfile>
+                </Route>
+                <Route path="/addproject">
+                    <AddProject></AddProject>
+                </Route>
+                <Route path="/exportproject">
+                    <ExportProject></ExportProject>
+                </Route>
+                <Route path="/recoverpassword">
+                    <RecoverPassword></RecoverPassword>
+                </Route>
+                <Route path="/setnewpassword">
+                    <SetNewPassword></SetNewPassword>
+                </Route>
+
+                <Route path="/invite">
+                    <Invite></Invite>
+                </Route>
+
+                <Route path="/login">
+                    <Login></Login>
+                </Route>
+                <Route path="/signup">
+                    <Signup></Signup>
+                </Route>
+                <Route path="/importproject">
+                    <ImportProject></ImportProject>
+                </Route>
+
+
+        </Router>
+    </>
 );
 
 export default App;

ff7b14ab5626f432b8f1b037067c37a531cd4dc6
Muhammad Umair <49072162+itskanny@users.noreply.github.com>
2022-03-13 01:35:59 +0500

    Some UI updates

--- src/Pages/AddProject/AddProject.js
+++ src/Pages/AddProject/AddProject.js
@@ -1,25 +1,43 @@
 import React from "react";
-import {Col} from "antd";
+import {Col, Row} from "antd";
 import AddProjectForm from "../../Components/Forms/AddProject/AddProjectForm";
 import "./AddProject.css";
+import Banner from "../../Components/Ui/Banner/Banner";
+import LoginForm from "../../Components/Forms/Login/LoginForm";
 
 const AddProject = () => {
     return (
-        <div>
+
+
+        <>
             <div>
                 <h3 className="h1">Create Project</h3>
-
             </div>
+            <Row align={"middle"} className={'tw-p-4 tw-pt-4  md:tw-flex-col lg:tw-flex-row tw-flex-col tw-justify-center tw-mx-2 sm:tw-mx-4 md:tw-mx-0 lg:tw-mx-20 xl:tw-mx-36 2xl:tw-mx-48'}>
 
-            <div className="center">
                 <Col className={"gutter-row tw-mt-10 md:tw-mt-0"} span={24} md={8}>
                     <AddProjectForm/>
                 </Col>
-            </div>
-            < div className=" flex justify-end ...">
+            </Row>
+        </>
 
-            </div>
-        </div>
+
+
+        // <div>
+        //     <div>
+        //         <h3 className="h1">Create Project</h3>
+        //
+        //     </div>
+        //
+        //     <Row className="center">
+        //         <Col className={"gutter-row tw-mt-10 md:tw-mt-0"} span={24} md={8}>
+        //             <AddProjectForm/>
+        //         </Col>
+        //     </Row>
+        //     < div className=" flex justify-end ...">
+        //
+        //     </div>
+        // </div>
     );
 };
 

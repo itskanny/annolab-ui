a1204c9636d4c7e33c2a0a83c7313c8038f2f2f9
Muhammad Umair <49072162+itskanny@users.noreply.github.com>
2022-04-24 23:58:26 +0500

    Team Created

---
+++ src/providers/TeamProvider.js
@@ -0,0 +1,29 @@
+import Form from "../utils/form";
+
+export class TeamProvider{
+    static create(data){
+        const formData = new Form(data)
+        return formData.post('teams/')
+            .then((response) => {
+                console.log(response)
+                return {
+                    data: response,
+                    hasErrors: false,
+                    status: response.status,
+                    statusText: response.statusText
+                }
+            })
+            .catch((error) => {
+                // console.log(error)
+                if (error.response.data.organization){
+                    error.response.data.non_field_errors = ["No organization has been created"]
+                }
+                return {
+                    data: error.response.data,
+                    hasErrors: true,
+                    status: error.response.status,
+                    statusText: error.response.statusText
+                }
+            })
+    }
+}

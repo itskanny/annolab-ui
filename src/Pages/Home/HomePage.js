9d04320163f202e0c5eafb7d2909a70b77afb5d6
Muhammad Umair <49072162+itskanny@users.noreply.github.com>
2022-04-27 16:24:17 +0500

    Project Listing done and backend connected

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -33,7 +33,7 @@ const Home = props => {
                  className={'tw-p-4 tw-pt-8 md:tw-h-full  md:tw-flex-col lg:tw-flex-row tw-flex-col tw-justify-center tw-mx-2 sm:tw-mx-4 md:tw-mx-6 lg:tw-mx-12 xl:tw-mx-20 2xl:tw-mx-28 tw-justify-center'}
 
             >
-                <Col span={24} md={8}>
+                <Col span={24} md={8} className={'tw-pr-3'}>
                     <Card >
                         <ListProjects data={DUMMY_DATA}/>
                     </Card>

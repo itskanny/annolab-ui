142ca7225370eb60fa6f5518d1e6c5525c17968c
Muhammad Umair <49072162+itskanny@users.noreply.github.com>
2022-05-23 01:33:08 +0500

    Opacity transitioned

--- src/Pages/Listing/ImageListing.js
+++ src/Pages/Listing/ImageListing.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 import ListPage from "../../Components/Functional/ListingTable/ListPage";
 import {ImageProvider} from "../../providers/ImageProvider";
 import AddImagesModelForm from "../../Components/Forms/ModelForms/ImageModalForms/AddImagesModelForm/AddImagesModelForm";
-import {Avatar, Button, List, Space, Tag} from "antd";
+import {Avatar, Button, Divider, List, Space, Tag} from "antd";
 import {formatDate} from "../../helpers/DataFormater";
 import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 import {openNotification} from "../../helpers/helper";
@@ -119,29 +119,49 @@ const ImageListing = (props) => {
     return (
 
         <>
-            {/*<AddImagesModelForm setRender={setRender} visible={visible} setVisible={setVisible} proj={props.proj}/>*/}
-            {/*<EditImageModalForm setRender={setRender} visible={editVisible} setVisible={setEditVisible} proj={props.proj}/>*/}
-            {/*<ListPage loading={loading} render={render} buttonHandler={openModelHandler} title={props.proj.name} tableType={'All Images'} buttonText={'Add Images'} columns={IMAGE_COLUMNS} fetcher={() => ImageProvider.fetchImages(props.proj.id)}/>*/}
+            <AddImagesModelForm setRender={setRender} visible={visible} setVisible={setVisible} proj={props.proj}/>
+            <EditImageModalForm setRender={setRender} visible={editVisible} setVisible={setEditVisible} proj={props.proj}/>
+            <ListPage loading={loading} render={render} buttonHandler={openModelHandler} title={props.proj.name} tableType={'All Images'} buttonText={'Add Images'} columns={IMAGE_COLUMNS} fetcher={() => ImageProvider.fetchImages(props.proj.id)}/>
 
 
             <div className={'tw-mx-16 tw-my-5'}>
 
                 <List
-                    grid={{ gutter: 16, column: 3 }}
+                    grid={{ gutter: 25, column: 3 }}
                     dataSource={data}
                     renderItem={item => (
                         <List.Item>
-                            <div className={'tw-bg-red-700 tw-h-56 tw-relative'}>
-                                <img className={'tw-w-full tw-h-full tw-object-cover'} src={item.image}/>
-                                <div className={'tw-absolute tw-bottom-0 tw-left-0 tw-right-0 tw-bg-black/70 tw-h-20'}>
+                            <div className={'tw-h-[250px] tw-relative tw-group'}>
+                                <img className={'tw-w-full tw-h-[200px] tw-absolute tw-object-cover tw-shadow-card-shadow tw-rounded-tl-[7px] tw-rounded-br-[7px] tw-rounded-tr-[25px] tw-rounded-bl-[25px] tw-scale-0 tw-opacity-100 group-hover:tw-opacity-50 tw-transition-all tw-duration-500 tw-ease-in-out'} src={item.image}/>
+
+                                <div className={'tw-absolute tw-left-2 tw-top-3 '}>
+                                    {/*<div className={'tw-rounded-[5px] tw-text-white tw-bg-amber/80 tw-border tw-border-amber tw-px-2 tw-py-0'}>*/}
+                                    {/*    Not Annotated*/}
+                                    {/*</div>*/}
+                                    <div className={'tw-rounded-[5px] tw-text-white tw-bg-primary/80 tw-border tw-border-primary tw-px-2 tw-py-0'}>
+                                        Annotated
+                                    </div>
+                                    {/*{item.is_annotated ?  <Tag color={"success"}>Annotated</Tag> : <Tag color={"processing"}>Not Annotated</Tag>}*/}
+                                </div>
+
+                                <div className={'tw-absolute tw-mx-[18px] tw-px-[20px] tw-py-[10px] tw-rounded-tl-[7px] tw-rounded-br-[7px] tw-rounded-tr-[25px] tw-rounded-bl-[25px] tw-shadow-card-shadow tw-bottom-0 tw-left-0 tw-right-0 tw-bg-white'}>
+                                    <p>{item.name}</p>
+                                    <div className={'tw-flex tw-justify-between'}>
+                                        <p className={'tw-mb-0'}>{formatDate(item.updated_date, 'LL')}</p>
+                                        <div className={'tw-rounded-[5px] tw-text-primary tw-bg-primary/10 tw-border tw-border-primary tw-px-2 tw-py-0'}>
+                                            Annotated
+                                        </div>
+                                    </div>
+
+                                    <div className={'tw-mt-2 tw-hidden tw-transition-all tw-duration-500 tw-ease-in-out group-hover:tw-block '}>
+                                        <div className={'tw-flex tw-justify-between'}>
+                                            <Button onClick={() => setEditVisible({state: true, row: item.id})} type="dashed" shape={'circle'} icon={<EditOutlined className={'tw-text-icon'}/>}/>
+                                            <Button onClick={() => deleteHandler(item.id)} type="dashed" shape={'circle'} icon={<DeleteOutlined style={{color: 'red'}}/>}/>
+                                        </div>
+                                    </div>
 
                                 </div>
-                                <div className={'tw-absolute tw-right-2 tw-top-3'}>
-                                    {/*{item.is_annotated ?  <Tag color={"success"}>Annotated</Tag> : <Tag color={"processing"}>Not Annotated</Tag>}*/}
-                                    <div className={'tw-p-0.5 tw-bg-primary/50 tw-text-pr tw-border tw-border-primary tw-rounded-md'}>
-                                        <b>Not Annotated</b>
-                                    </div>
-                                </div>
+
                             </div>
                         </List.Item>
                     )}
@@ -149,49 +169,6 @@ const ImageListing = (props) => {
 
 
             </div>
-
-            {/*<div className={'tw-grid tw-grid-cols-3 tw-h-full tw-gap-3 '}>*/}
-            {/*    <div className={'tw-bg-red-700 tw-h-56 tw-relative'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/organization_avatars/1_7_2ewoOpw.jpg"}/>*/}
-            {/*        <div className={'tw-absolute tw-bottom-0 tw-left-0 tw-right-0 tw-bg-black/70 tw-h-20'}>*/}
-
-            {/*        </div>*/}
-            {/*        <Tag color={"processing"}>Not Annotated</Tag>*/}
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-yellow-400 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/avatars/1_9_GV0nEyy.jpg"}/>*/}
-
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-blue-600 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={'http://localhost:8000/media/project_avatars/1_7_JXIq5uG.jpg'}/>*/}
-
-            {/*    </div>*/}
-
-            {/*    <div className={'tw-bg-red-700 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/organization_avatars/1_7_2ewoOpw.jpg"}/>*/}
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-yellow-400 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/avatars/1_9_GV0nEyy.jpg"}/>*/}
-
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-blue-600 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={'http://localhost:8000/media/project_avatars/1_7_JXIq5uG.jpg'}/>*/}
-
-            {/*    </div>*/}
-
-            {/*    <div className={'tw-bg-red-700 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/organization_avatars/1_7_2ewoOpw.jpg"}/>*/}
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-yellow-400 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={"http://localhost:8000/media/avatars/1_9_GV0nEyy.jpg"}/>*/}
-
-            {/*    </div>*/}
-            {/*    <div className={'tw-bg-blue-600 tw-h-56'}>*/}
-            {/*        <img className={'tw-w-full tw-h-full tw-object-cover'} src={'http://localhost:8000/media/project_avatars/1_7_JXIq5uG.jpg'}/>*/}
-
-            {/*    </div>*/}
-
-            {/*</div>*/}
         </>
     )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,15 @@ module.exports = {
         "background": "#ffffff",
         "icon": "#9A9AAF",
         "item-hover": "#f5f5f5",
-        "border-color": "#e2e0ff"
+        "border-color": "#e2e0ff",
+        "amber": "#FFB800",
       },
       boxShadow: {
         // '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
         "input-icon":"12px 0 10px -10px rgba(0,0,0,.05);",
         "input-icon-suffix":"-12px 0 10px -10px rgba(0,0,0,.05);",
-        "input-icon-focus":" rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.25) 0px 8px 32px;"
+        "input-icon-focus":" rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.25) 0px 8px 32px;",
+        "card-shadow": " 0px 4px 20px -4px rgba(0,0,0,0.1);",
       },
       screens: {
         'xs': '480px',
